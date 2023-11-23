5eedfe29f96dab003b6b1a61378c2bf1613e7f7b
Hakan Tura <Hakan.tura@gmx.net>
2023-11-23 09:22:22 +0100

    001_JS_Variablen_done

---
+++ 001_JS_Variablen/script.js
@@ -0,0 +1,50 @@
+Zewas = "Guten Tag"
+
+console.log(Zewas)
+
+
+///Variablen///
+
+let a = 7
+let b = 8 
+let c = 10
+
+
+let result = a+b+c
+
+console.log("Das Ergebnis ist " + result)
+
+let result2 = (a+b)/c
+console.log("Das Ergebnis ist " + result2)
+let result3 = c-a+b
+console.log("Das Ergebnis ist " + result3)
+let result4 = a+b+c*3
+console.log("Das Ergebnis ist " + result4)
+let result5 = (a+b+c)/4
+console.log("Das Ergebnis ist " + result5)
+
+let a1 = 3
+let b1 = a+3
+let c1 = c-2
+
+/// Zweite Aufgabe///
+let result6 = a1+b1+c1
+
+console.log("Das Ergebnis ist " + result6)
+
+let result7 = (a1+b1)/c1
+console.log("Das Ergebnis ist " + result7)
+let result8 = c1-a1+b1
+console.log("Das Ergebnis ist " + result8)
+let result9 = a1+b1+c1*3
+console.log("Das Ergebnis ist " + result9)
+let result10 = (a1+b1+c1)/4
+console.log("Das Ergebnis ist " + result10)
+
+
+
+// Ändere die Variablen auf folgende Werte
+// a = 3
+// b = a + 3
+// c = c -3
+// gib die neuen Werte der Variablen aus
