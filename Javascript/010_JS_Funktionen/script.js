bc4ea81e8836f1fc60a65d4ed66640fad7a8cdc2
Hakan Tura <Hakan.tura@gmx.net>
2024-03-07 09:22:26 +0100

    010_JS_Funktionen_done

--- Javascript/010_JS_Funktionen/script.js
+++ Javascript/010_JS_Funktionen/script.js
@@ -1,36 +1,34 @@
-// Funktion zum Addieren von zwei Zahlen
-function addieren(a, b) {
-    return a + b;
-  }
-  
-  // Funktion zum Subtrahieren von zwei Zahlen
-  function subtraktion(a, b) {
-    return a - b;
-  }
-  
-  // Funktion zum Multiplizieren von zwei Zahlen
-  function multiplikation(a, b) {
-    return a * b;
-  }
-  
-  // Funktion für die Superberechnung (a+b)/2 * a
-  function supercalculation(a, b) {
-    return ((a + b) / 2) * a;
-  }
-
-  /// Ostern 
-
-  function Berechnungostern(year) {
-    const a = year % 19;
-    const b = Math.floor((7 * a + 1) / 19);
-    const m = Math.floor((11 * a + 4 - b) / 29);
-    const q = Math.floor(year / 4);
-    const w = Math.floor((year + q + 31 - m) / 7);
-    const p = 25 - m - w;
-  
-    if (p > 0) {
-      return new Date(year, 3, p - 1);
-    } else {
-      return new Date(year, 2, 22 + p);
-    }
-  } 
+let a = 7;
+let b = 4;
+ 
+function add(a,b){
+    return a + b
+}  
+let result = add(a,b);
+console.log(result);
+ 
+function subtract(a,b){
+    return a - b
+}
+let result2 = subtract(a,b);
+console.log(result2);
+ 
+function mulitply(a,b){
+    return a * b
+}
+let result3 = mulitply(a,b);
+console.log(result3);
+ 
+function supercalculation(a,b){
+    return (a+b)/2*a
+}
+let result4 = supercalculation(a,b);
+console.log(result4);
+ 
+let year = 31.03;
+function printEasterDate(year){
+    return year
+}
+let result5 = printEasterDate(year);
+console.log (result5);
+ 
